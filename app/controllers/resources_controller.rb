832e222177ba40126f32844aea13923311ca28cc
faatehim <faatehim@gmail.com>
2017-04-19 13:24:05 -0400

    Added form to submit resource and index page now shows all resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,7 +1,21 @@
 class ResourcesController < ApplicationController
   def index
+  	@resources = Resource.all
   end
 
   def new
+  	@resource = Resource.new
   end
+
+
+  def create
+  	@resource= Resource.new(params.require(:resource).permit(:title, :description, :url))
+
+  	if @resource.save
+    	redirect_to root_path
+     else
+     render "new"
+  end
+end
+
 end
