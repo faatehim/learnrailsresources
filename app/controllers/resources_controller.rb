dd2127214c4814bdca2bcdebbf69aeb0d8bd5748
faatehim <faatehim@gmail.com>
2017-04-22 20:40:53 -0400

    Finished Basic CRUD functionality

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -22,37 +22,49 @@ class ResourcesController < ApplicationController
     @resource = Resource.where(:id => params[:id]).first
 
     respond_to do |format|
-      if @resource.update_attributes(params[:resource])
+      if @resource.update_attributes(resources_params)
         format.html { redirect_to @resource, :notice => 'Resource was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
         format.json { render json: @resource.errors, :status => :unprocessable_entity }
       end
+      end
+  end
+
+
+ def destroy
+ 	@toDelete = Resource.where(:id => params[:id]).first
+ 	puts @toDelete.id
+
+    if @toDelete.destroy
+      flash[:notice] = "Successfully deleted post!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Error updating post!"
     end
   end
 
 
-  def destroy
-    @resource = Resource.where(:id => params[:id]).first
-  	@resource.destroy
-
-  respond_to do |format|
-    format.html { redirect_to resource_url }
-    format.json { head :no_content }
-  end
-end
-
-
 
   def create
-  	@resource= Resource.new
+  	@resource= Resource.new(resources_params)
 
   	if @resource.save
     	redirect_to root_path
+    	flash[:alert] = "Successfully created new resource!"
      else
        render "new"
      end
     end
 
+end
+
+
+
+  private
+
+
+ def resources_params
+ 	    params.require(:resource).permit(:title, :url, :description)
 end
